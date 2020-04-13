287288add3e8ae570b45581a5abc1a3e79acd0ea
Joseph Baca <josephcbaca@gmail.com>
2020-04-12 18:14:16 -0700

    Initial commit - currently does not write password to page

---
+++ README.md
@@ -0,0 +1,13 @@
+# Password Generator
+
+This is a standard password generator that will allow a user to select the length of the password between 8 and 52 characters and then from the following critiria thereafter:
+
+Would you like to use lowercase letters?
+Would you like to use uppercase letters?
+Would you like to use numbers?
+Would you like to use special characters?
+
+
+Use this password generator for a completely randomized a password to help you boost security.
+
+Enjoy!
---
+++ script.js
@@ -0,0 +1,82 @@
+// Assignment Code
+let generateBtn = document.querySelector("#generate");
+
+let amountCharacters = prompt("Between 8 and 52 characters, how long would you like your Password?")
+
+let numArray = ['0','1','2','3','4','5','6','7','8','9'];
+let alphaArray = ['a','b','c','d','e','f','g','h','i','k','l','m','n','p','q','r','s','t','u','v','w','x','y','z'];
+let specialArray = ['!','#','$','%','^','*',','];
+let upperAlphaArray = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+let mainArray = [];
+
+
+if (amountCharacters < 8) {
+  alert("Password must be 8 characters or larger.")
+}
+else if (amountCharacters > 52){
+  alert("Password must be no larger than 52 characters.")
+}
+else {
+  let confirmNums = confirm("Would you like to use Numbers?")
+  let confirmAlpha = confirm("Would you like to use Lower Cased Letters?")
+  let confirmUpperAlpha = confirm("Would you like to use Upper Cased Letters?")
+  let confirmSpecial = confirm("Would you like to use Special Characters?")
+
+
+  let generateString = function(confirmNums, confirmAlpha, confirmUpperAlpha, confirmSpecial) {
+
+
+    if (confirmNums) {
+      mainArray.push(numArray);
+    }
+    if (confirmAlpha) {
+      mainArray.push(alphaArray);
+    }
+    if (confirmUpperAlpha) {
+      mainArray.push(upperAlphaArray);
+    }
+    if (confirmSpecial) {
+      mainArray.push(specialArray);
+    }
+    return mainArray
+  }
+
+generateString(confirmNums, confirmAlpha, confirmUpperAlpha, confirmSpecial)
+
+}
+
+//First Loop randomizes between arrays returned as true through user selection, second loop randomizes within the array selected in first loop. 
+function getRandomChar() {
+    for (let i = 0; i < mainArray.length; i++) {
+      let outsideArray = Math.floor(Math.random() * mainArray.length);
+      const insideArray = mainArray[outsideArray];
+
+        for (let j = 0; j < insideArray.length; j++) {
+          let outsideArray = Math.floor(Math.random() * insideArray.length)
+          const insideElement = insideArray[outsideArray];
+
+          return insideElement;
+        }
+    }
+  }
+
+//Will randomize the one Random Character we get from above loop within loop
+for (let k = 0; k < amountCharacters; k++) {
+  let randPassword = getRandomChar();
+  console.log(randPassword);
+};
+
+
+// Write password to the #password input
+function writePassword() {
+  let password = generatePassword();
+  let passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
+// Add event listener to generate button
+generateBtn.addEventListener("click", writePassword);
+
+
